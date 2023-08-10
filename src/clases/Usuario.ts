7ede32967e89498b156307e6a2fbaee22b225f15
valentincoletta322 <81708912+valentincoletta322@users.noreply.github.com>
2023-08-10 19:34:49 -0300

    update Usuario.ts

--- src/clases/Usuario.ts
+++ src/clases/Usuario.ts
@@ -1,19 +1,19 @@
 export class Usuario{
     nombre: String;
     apellido: String;
-    DNI: String;
+    // DNI: String;
     fechaNac: Date;
     fotoDoc: String;
     mail: String;
     contraseña: String;
     estado: String;
 
-    constructor(contraseña: String, nombre: String, mail: String, DNI: String,
+    constructor(contraseña: String, nombre: String, mail: String, /*DNI: String*/,
         fechaNac: Date, estado: String, apellido: String, fotoDoc: String){
         this.contraseña = contraseña;
         this.nombre = nombre;
         this.mail = mail;
-        this.DNI = DNI;
+        // this.DNI = DNI;
         this.fechaNac = fechaNac;
         this.estado = estado;
         this.apellido = apellido;
